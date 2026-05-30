f436900dea333316e7db6101736a47151ef06b60
duaspahiu <duaspahiu5@gmail.com>
2026-05-30 14:41:32 +0200

    lesson 1

---
+++ pythonProject1/lesson 1/list.py
@@ -0,0 +1,25 @@
+stundent = ("eldoni", "suela", "rudina","liza","dua","ylli","uniku,","trimi"]
+
+print(stundent)
+
+print(stundent[0])
+print(stundent[1])
+print(stundent[8])
+
+#stundentat.reverse()
+
+
+stundent.append("dua")
+stundent.insert()
+
+
+
+print(stundent.count("dua"))
+stundent.connet("dua")
+
+lista2 = stundent.copy()
+lista2.clear()
+print(lista2)
+print("dua eshte ne pozicion e :",stundent.index("dua"))
+
+print(stundent)
---
+++ pythonProject1/lesson 1/main.py
@@ -0,0 +1,20 @@
+name = "dua" # string
+
+age = 19 # ky eshte nje int
+
+#print(name)
+#print(age)
+
+x = 10
+y=5
+y="15"
+
+e = int(z)
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+print(type(z))
+print(type(e))
